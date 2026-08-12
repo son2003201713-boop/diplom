0a7b0cd77198169a0d5a69ae9c388ef222fea9cd
son2003201713-boop <son2003201713@gmail.com>
2026-08-12 17:35:20 +0300

    обновила сайт

--- lib/content.ts
+++ lib/content.ts
@@ -60,7 +60,7 @@ export const PROTAGONISTS = [
   },
   {
     num: "03",
-    name: "DIVA ETHNO FUTURE SOUND",
+    name: "Мăнкун",
     role: "МУЗЫКА",
     image: "/images/diva.jpg",
     text: "Народные интонации встречаются с современной музыкальной аранжировкой.",
