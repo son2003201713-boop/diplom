bd8ae4ee610a0033fa13bdbb633f5f81aedc739e
son2003201713-boop <son2003201713@gmail.com>
2026-08-15 20:00:01 +0300

    подключила Яндекс Метрику

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script"
 import { Analytics } from "@vercel/analytics/next"
 import type { Metadata, Viewport } from "next"
 import { Playfair_Display, Inter } from "next/font/google"
@@ -42,9 +43,50 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ru" className={`${playfair.variable} ${inter.variable} bg-background`}>
+    <html
+      lang="ru"
+      className={`${playfair.variable} ${inter.variable} bg-background`}
+    >
       <body className="antialiased font-sans">
         {children}
+
+        {/* Яндекс Метрика */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+              m[i]=m[i]||function(){
+                (m[i].a=m[i].a||[]).push(arguments)
+              };
+              m[i].l=1*new Date();
+
+              for (var j=0; j<document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                  return;
+                }
+              }
+
+              k=e.createElement(t);
+              a=e.getElementsByTagName(t)[0];
+              k.async=1;
+              k.src=r;
+              a.parentNode.insertBefore(k,a);
+            })(
+              window,
+              document,
+              "script",
+              "https://mc.yandex.ru/metrika/tag.js?id=111635291",
+              "ym"
+            );
+
+            ym(111635291, "init", {
+              clickmap: true,
+              trackLinks: true,
+              accurateTrackBounce: true,
+              webvisor: true
+            });
+          `}
+        </Script>
+
         {process.env.NODE_ENV === "production" && <Analytics />}
       </body>
     </html>
